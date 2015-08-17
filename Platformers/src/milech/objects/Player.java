58ec0e56ab4382becf8c821f51197551a8225cc0
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-08-17 13:38:53 +0200

    Add collicion regions to Player.
    Signed-off-by: Mikolaj Lech <mikolajj.lech@gmail.com>

--- Platformers/src/milech/objects/Player.java
+++ Platformers/src/milech/objects/Player.java
@@ -2,6 +2,7 @@ package milech.objects;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 
@@ -10,7 +11,7 @@ import milech.framework.ObjectId;
 
 public class Player extends GameObject{
 	
-	private float width = 32, height = 64;
+	private float width = 48, height = 96;
 	private float gravity = 0.05f; 
 	private final float MAX_SPEED = 10;
 	
@@ -20,7 +21,7 @@ public class Player extends GameObject{
 
 	public void tick(LinkedList<GameObject> object) {
 		x += velX;
-		y += velY;
+//		y += velY;
 		if(falling || jumping) {
 			velY += gravity;
 			if(velY > MAX_SPEED) {
@@ -32,10 +33,31 @@ public class Player extends GameObject{
 	public void render(Graphics g) {
 		g.setColor(Color.blue);
 		g.fillRect((int)x, (int)y, (int)width, (int)height);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		g.setColor(Color.red);
+		g2d.draw(getBoundsDown());
+		g2d.draw(getBoundsRight());
+		g2d.draw(getBoundsLeft());
+		g2d.draw(getBoundsTop());
+	}
+
+	public Rectangle getBoundsDown() {
+		return new Rectangle((int) ((int)x + (width/2 - ((width/2)/2))), (int)y + (int)(height / 2), (int)width / 2, (int)height / 2);
+	}
+	public Rectangle getBoundsTop() {
+		return new Rectangle((int) ((int)x + (width/2) - ((width/2)/2)), (int)y, (int)width / 2, (int)height / 2);
+	}
+	public Rectangle getBoundsRight() {
+		return new Rectangle((int) ((int)x + width - 5), (int)y + 5, (int)5, (int)height - 10);
+	}
+	public Rectangle getBoundsLeft() {
+		return new Rectangle((int)x, (int)y + 5, (int)5, (int)height - 10);
 	}
 
 	public Rectangle getBounds() {
 		return new Rectangle((int)x, (int)y, (int)width, (int)height);
+
 	}
 	
 
